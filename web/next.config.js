c8706f963b53e1b23bee1623b1d0376a08717aa6
Arthur Soares <arthursks7@gmail.com>
2023-11-07 14:35:24 -0300

    client

---
+++ web/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["avatars.githubusercontent.com"],
+  },
+};
+
+// eslint-disable-next-line no-undef
+module.exports = nextConfig;
---
+++ web/src/app/layout.tsx
@@ -0,0 +1,33 @@
+import "./globals.css";
+import { ReactNode } from "react";
+import React from 'react';
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjuree,
+} from "next/font/google";
+
+const roboto = Roboto({ subsets: ["latin"], variable: "--font-roboto" });
+
+const baiJamjuree = BaiJamjuree({
+  subsets: ["latin"],
+  weight: "700",
+  variable: "--font-bai-jamjuree",
+});
+
+export const metadata = {
+  title: "NLW Spacetime",
+  description:
+    "Uma cápsula do tempo construida com React, Next.js, TailwindCSS e TypeScript",
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
