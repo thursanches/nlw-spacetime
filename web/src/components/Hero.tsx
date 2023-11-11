c7e9447370eebec0e5f19a09c9017df9747cdf22
Arthur Soares <arthursks7@gmail.com>
2023-11-11 18:15:01 -0300

    a to Link

--- web/src/components/Hero.tsx
+++ web/src/components/Hero.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 
 import nlwLogo from "../assets/nlw-spacetime-logo.svg";
+import Link from "next/link";
 
 export function Hero() {
   return (
@@ -18,12 +19,12 @@ export function Hero() {
         </p>
       </div>
 
-      <a
-        href=""
+      <Link
+        href="/memories/new"
         className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
       >
         CADASTRAR LEMBRANÇA
-      </a>
+      </Link>
     </div>
   );
 }
