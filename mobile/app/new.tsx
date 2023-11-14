355859b9fb7247cf0fecc1e74040a79b4574089e
Arthur Soares <arthursks7@gmail.com>
2023-11-14 14:45:37 -0300

    add text area and button

--- mobile/app/new.tsx
+++ mobile/app/new.tsx
@@ -1,10 +1,11 @@
-import { Text, TouchableOpacity, View, Switch } from "react-native";
+import { Text, TouchableOpacity, ScrollView, View, Switch } from "react-native";
 import Icon from '@expo/vector-icons/Feather'
 
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 import { Link } from "expo-router";
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { useState } from "react";
+import { TextInput } from "react-native-gesture-handler";
 
 export default function NewMemory() {
   const { bottom, top } = useSafeAreaInsets()
@@ -12,8 +13,8 @@ export default function NewMemory() {
   const [isPublic, setIsPublic] = useState(false)
 
   return (
-    <View className="flex-1 px-8"
-    style={{ paddingBottom: bottom, paddingTop: top }}
+    <ScrollView className="flex-1 px-8"
+    contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
     >
       <View className="mt-4 flex-row items-center justify-between"> 
         <NLWLogo /> 
@@ -24,7 +25,7 @@ export default function NewMemory() {
           </TouchableOpacity>
         </Link>
       </View>
-      
+
       <View className="mt-6 space-y-6">
         <View className="flex-row items-center gap-2">
           <Switch 
@@ -35,7 +36,11 @@ export default function NewMemory() {
           />
           <Text className="font-body text-base text-gray-200">Tornar memória pública</Text>
         </View>
-        <TouchableOpacity className="h-32 justify-center rounded-lg border border-dashed border-gray-500 bg-black/20">
+        
+        <TouchableOpacity 
+          activeOpacity={0.7} 
+          className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20"
+        >
           <View className="flex-row items-center gap-2">
             <Icon name="image" color="#FFF" />
             <Text className="font-body text-sm text-gray-200">
@@ -43,7 +48,21 @@ export default function NewMemory() {
             </Text>
           </View>
         </TouchableOpacity>
+
+        <TextInput 
+        multiline
+        className="p-0 font-body text-lg text-gray-50"
+        placeholderTextColor="#56565a"
+        placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."/>
+
+        <TouchableOpacity 
+          activeOpacity={0.7}className="items-center self-end rounded-full bg-green-500 px-5 py-2" 
+        >
+          <Text className="font-alt text-sm uppercase  text-black">
+            Salvar
+          </Text>
+        </TouchableOpacity>
       </View>
-    </View>
+    </ScrollView>
   )
 }
