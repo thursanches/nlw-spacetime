ff55b86253fe3c03633d88d7239a7acdba7cc21e
Arthur Soares <arthursks7@gmail.com>
2023-11-18 16:02:29 -0300

    input name add

--- web/src/components/MediaPicker.tsx
+++ web/src/components/MediaPicker.tsx
@@ -13,20 +13,23 @@ export function MediaPicker() {
     }
 
     const previewURL = URL.createObjectURL(files[0]);
+
     setPreview(previewURL);
   }
 
   return (
     <>
       <input
-        onChange={onFileSelected}
         type="file"
         id="media"
-        accept="image/*"
+        name="coverUrl"
         className="invisible h-0 w-0"
+        accept="image/*"
+        onChange={onFileSelected}
       />
 
       {preview && (
+        // eslint-disable-next-line
         <img
           src={preview}
           alt=""
