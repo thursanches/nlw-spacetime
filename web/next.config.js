1a8d1da241708bcdd3835f281cc254031547e528
Arthur Soares <arthursks7@gmail.com>
2023-11-21 23:45:27 -0300

    next 14

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["avatars.githubusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '192.168.1.8',
+        port: '3333',
+        pathname: '/uploads/**',
+      },
+    ],
+    domains: ["avatars.githubusercontent.com", "192.168.1.8"],
   },
 };
 
