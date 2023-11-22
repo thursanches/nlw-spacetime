3ccb64105f740900987865d02ef484d2190c19d5
Arthur Soares <arthursks7@gmail.com>
2023-11-21 23:48:45 -0300

    Final Empty Memories

--- web/src/components/EmptyMemories.tsx
+++ web/src/components/EmptyMemories.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function EmptyMemories() {
   return (
-    <div className="flex flex-1 items-center justify-center">
+    <div className="flex flex-1 items-center justify-center p-16">
       <p className="w-[360px] text-center leading-relaxed">
         Você ainda não registrou nenhuma lembrança, começe a {""}
         <a href="" className="underline hover:text-gray-50">
